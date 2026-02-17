9d077edc18fcba6ba2aad1ca5a39e63bb042fd96
Leonardo P Wistra <leowistra@gmail.com>
2026-02-17 15:27:18 -0300

    Primeiro teste automatizado de login

---
+++ test/specs/login.spec.js
@@ -0,0 +1,22 @@
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', async () => {
+        await $('~Login').click()
+        await $('~input-email').setValue('teste@teste.com')
+        await $('~input-password').setValue('senha@123')
+        await $('~button-LOGIN').click()
+        expect(await $('id=android:id/message')).toBeDisplayed()
+        await driver.acceptAlert()
+    });
+
+    it('Deve falhar ao fazer login com email inválido', async () => {
+        await $('~Login').click()
+        await $('~input-email').setValue('teste@teste')
+        await $('~input-password').setValue('senha@123')
+        await $('~button-LOGIN').click()
+        const mensagem = await $('//android.widget.TextView[@text="Please enter a valid email address"]')
+        expect(mensagem).toBeDisplayed()
+        await driver.pause(5000)
+    });
+
+});
